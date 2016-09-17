58d1184d659f1809e2f0f36e3e7c47f506923444
SkywAlKer <ggqq12356@yahoo.com.tw>
2016-09-18 00:52:36 +0800

    Jquery Game

--- control.js
+++ control.js
@@ -1,29 +1,45 @@
-$(document).ready(function(){
-	$toggle_on = $(".fa-toggle-on");
-	$toggle_off = $(".fa-toggle-off");
+	$(document).ready(function(){
+		var $body = $("body"),
+			$toggle_on = $(".fa-toggle-on"),
+			$toggle_off = $(".fa-toggle-off"),
+			$icon = $(".icon");
 
-	$bt = $(".bt");
-	$icon = $(".icon");
-
-	$toggle_on.hide();
-	$icon.hide();
-
-	$toggle_off.click(function(){
-		$(this).hide();
-		$toggle_on.show();
-		$icon.show();
-	});
-
-	$toggle_on.click(function(){
-		$(this).hide();
-		$toggle_off.show();
+		//預設狀態
+		$toggle_on.hide();
 		$icon.hide();
+
+		$toggle_off.click(function(){
+			$(this).hide();
+			$toggle_on.show();
+			$icon.show();
+		});
+
+		$toggle_on.click(function(){
+			$(this).hide();
+			$toggle_off.show();
+			$icon.hide();
+		});
+
+		$fun = $(".fun");
+
+		$fun.click(function(){
+			alert("蛤？");
+		})
+
+		//鎖定右鍵選單
+        $body.contextmenu(function(e){
+            e.preventDefault();
+        });
+
+		//判斷裝置種類
+	    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	    	console.log("#Device : Mobile");
+	    }
+	    else {
+	        console.log("#Device : PC");
+	        $body.css({
+	        	"margin-left":"35%"
+	        })
+	    }
+
 	});
-
-	$fun = $(".fun");
-
-	$fun.click(function(){
-		alert("蛤？");
-	})
-
-});
