6b2d690d8c1baf605e55201791956f702f31fb9c
SkywAlKer <ggqq12356@yahoo.com.tw>
2016-10-07 17:28:34 +0800

    web in juery

--- fate/control.js
+++ fate/control.js
@@ -14,14 +14,17 @@ $(document).ready(function(){
 
     //參數
     var contain = $(".contain")
-        img = $(".img")
         ball = $(".ball")
         card = $(".card")
+        cleaner = $(".cleaner")
         r_max = 0
         t = 0
-    	player = ["Bulbasaur", "Dragonite", "Hoothoot", "Kingler", "Plusle_Minun"]
-        player_c = ["妙蛙種子", "快龍", "咕咕", "巨鉗蟹", "正負拍拍"]
+        //"Plusle_Minun", "正負拍拍"
+    	player = ["Bulbasaur", "Dragonite", "Hoothoot", "Kingler", "Zubat", "Jynx"]
+        player_c = ["妙蛙種子", "快龍", "咕咕", "巨鉗蟹", "超音蝠", "迷唇姐"]
         punishment = ["用屁股寫名字", "畫臉", "跳繩", "交互蹲跳", "夾夾子", "跑操場"]
+        player_max = 10
+        player_list = []
 
     function randomNum(r_max){
         var x = Math.floor(Math.random() * r_max)
@@ -34,27 +37,42 @@ $(document).ready(function(){
 
     ball.click(function(){
         var r_max = player.length
-            p = randomNum(r_max)
+            imagegroup = $(".image-group")
             cardcontain = $(".cardcontain")
-            imagecontain = $(".imagecontain")
 
         cardcontain.remove()
-        imagecontain.remove()
-        img.css({"background-image":"url("+player[p]+".png)"})
-        contain.append("<div class='imagecontain'>"+player_c[p]+"</div>")
+        imagegroup.remove()
+
+        for (var i = 0; i < player_max; i++) {
+            var p = randomNum(r_max)
+            player_list[i] = player_c[p]
+
+            contain.append("<div class='image-group image"+i+"'>"+"("+(i+1)+")"+player_c[p]+"</div>")
+            var image = $(".image"+i)
+            image.css({"background-image":"url("+player[p]+".png)"})
+        }
         //console.log('p:', p, 't:', t, player_c[p])
+        console.log('player_list:', player_list)
     })
 
     card.click(function(){
         var r_max = punishment.length
             p = randomNum(r_max)
+            imagegroup = $(".image-group")
             cardcontain = $(".cardcontain")
-            imagecontain = $(".imagecontain")
 
         cardcontain.remove()
-        imagecontain.remove()
-        img.css({"background-image":"url()"})
+        imagegroup.remove()
         contain.append("<div class='cardcontain'>"+punishment[p]+"</div>")
         //console.log('p:', p, 't:', t, punishment[p])
+        console.log('punishment:', punishment[p])
+    })
+
+    cleaner.click(function(){
+        var imagegroup = $(".image-group")
+            cardcontain = $(".cardcontain")
+
+        cardcontain.remove()
+        imagegroup.remove()
     })
 })
