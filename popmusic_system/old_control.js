3cc02f817fe8748ce2c68b0df4484e078ba7fe18
ggqq12356 <ggqq12356@yahoo.com.tw>
2017-07-19 22:03:37 +0800

    20170719-popmusic-system

--- popmusic_system/old_control.js
+++ popmusic_system/old_control.js
@@ -10,10 +10,7 @@ $(document).ready(function() {
 	//判斷裝置種類
     if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     	console.log("#Device : Mobile")
-
-		//MainWidth = window.innerWidth/1.2
-    	//$('.main').css({"width":MainWidth})
-
+		
 		CalendarWidth = 600
 		//$('#calendar').css({"width":CalendarWidth})
 
@@ -34,13 +31,9 @@ $(document).ready(function() {
     else {
         console.log("#Device : PC")
 
-		//MainWidth = window.innerWidth/1.2
-    	//$('.main').css({"width":MainWidth})
-
 		$('.tp1').css({"position":"absolute"})
 
 		tp2Width = (window.innerWidth)/5
-		//tp2Width = $('.tp1').css("left")
 		$('.tp2').css({"margin-left":tp2Width})
 
 		$('.tp2').append('<div class="excel-table"><iframe src="https://docs.google.com/spreadsheets/d/1yS-MW2BMESK6o5_-qYwzNi1BPu7kmtx3w7JqAF1wO0M/pubhtml?widget=true&amp;headers=false" class="excel"></iframe></div>')
@@ -53,27 +46,6 @@ $(document).ready(function() {
     //鎖定右鍵選單
     $("body").contextmenu(function(e){
         e.preventDefault()
-        /*
-        bootbox.confirm({
-		    message: "是否刪除?",
-		    buttons: {
-		        confirm: {
-		            label: '是',
-		            className: 'btn-success'
-		        },
-		        cancel: {
-		            label: '否',
-		            className: 'btn-danger'
-		        }
-		    },
-		    callback: function (result) {
-		    	if(result==true) r = "是"
-				else r = "否"
-
-		        console.log('[訊息] ' + r)
-		    }
-		})
-		*/
     })
 
 	var styles = 'background: #f0f; color: #fff; padding: 0 100px; font-size: 30px;'
@@ -107,7 +79,6 @@ $(document).ready(function() {
 	spreadsheetId = '1yS-MW2BMESK6o5_-qYwzNi1BPu7kmtx3w7JqAF1wO0M'
 	range = 'A1'
 
-	GoogleAppScript = "https://script.google.com/macros/s/AKfycbwXe1UlNdLZErA5ouR45DDPCw9ZctBj9CIoahuQL6rhvcTPCTJj/exec"
 
 	Select_Events = []
 	newName = ''
@@ -119,110 +90,19 @@ $(document).ready(function() {
 	slt_st_full = ''
 	slt_end_full = ''
 
-	/*
-	$('.buttons').append("<button class='read'>read</button>")
-	$('.read').click(function(){
-		newData = {"method":"read"}
-		$.ajax({
-    		url: GoogleAppScript,
-    		type: "GET",
-    		data: newData,
-    		success: function(result){
-    			//var data = JSON.stringify(result)
-    			var data = result.split(',')
-    			var data_array = []
+    //儲存行事曆 送出表單
+    $('.fc-left').append('<button type="button" class="save-calendar fc-button fc-state-default fc-corner-left fc-corner-right">儲存</button>')
 
-    			//列Rows
-    			//行Columns
-    			//產生列次數 = 筆資料
+    $('.fc-button').mouseenter(function(){$(this).addClass('fc-state-hover')})
+    $('.fc-button').mouseleave(function(){$(this).removeClass('fc-state-hover')})
 
-    			//234
-    			var data_length = data.length
-    			//234/6 = 39
-    			var data_rows = data.length/6
+    $('.fc-button').mousedown(function(){$(this).addClass('fc-state-down')})
+    $('.fc-button').mouseup(function(){$(this).removeClass('fc-state-down')})
 
-    			console.log(data_length)
-				console.log(data_rows)
-
-    			var i=0, j=0;
-    			//i=0 -> data_rows 39
-    			//j=0 -> 6
-    			for(k=0 ; k<data_length ; k++){
-
-    				/*
-    				if(j==3||j==4||j==5){
-    					data_array[i][j] = data[k].slice(0, -5)
-    				}
-    				else{
-    					//data_array[i][j] = data[k] //###bug
-    				}
-
-    				console.log("j", j)
-    				console.log("i", i)
-
-
-    				if(j<6){
-    					j++
-    				}
-    				else{
-    					j=0
-    					if(i<data_rows) i++
-    					else i=0
-    				}
-
-    			}
-
-    			//console.log(data_array)
-
-    		}
-    	})
-	})
-	*/
-
-	/*
-	$('.buttons').append("<button class='update'>update</button>")
-	$('.update').click(function(){
-
-		newData = {
-			"method":"update"
-		}
-
-		$.ajax({
-    		url: GoogleAppScript,
-    		type: "POST",
-    		data: newData,
-    		success: function(result){
-    			console.log(result)
-    		}
-    	})
-
-	})
-
-	$('.buttons').append("<button class='delete'>delete</button>")
-	$('.delete').click(function(){
-
-		newData = {
-			"method":"delete"
-		}
-
-		$.ajax({
-    		url: GoogleAppScript,
-    		type: "POST",
-    		data: newData,
-    		success: function(result){
-    			console.log(result)
-    		}
-    	})
-
-	})
-	*/
-	
     //-----------------Full Calendar---------------
 
     $('#calendar').fullCalendar({
 
-
-    	//行事曆設定
     	header:{
     		left:   'title',
 		    center: '',
@@ -246,62 +126,46 @@ $(document).ready(function() {
 
         //timezone: 'UTC',
 
-
-
-        //按下
-        eventClick: function(event, element, view) {
+        /*
+        //Google Calendar
+        googleCalendarApiKey: 'AIzaSyBRrsxwlgFO9jvq-QtvciT3zhUv60U1apA',
+        events: {
         	
+            googleCalendarId: 'ggqqqqgg12356@gmail.com',
+		    color: 'yellow',
+		    textColor: 'black',
+		    editable: true,
+        },
+
+		events: [
+			{
+			    title: 'Neverland',
+			    start: '2017-07-05T10:00',
+			    end:   '2017-07-05T12:00',
+			    color: 'yellow',
+				textColor: 'black',
+			    editable: true,
+			},
+			{
+			    title: 'Babyband',
+			    start: '2017-07-06T10:00',
+			    end:   '2017-07-06T12:00',
+			    color: 'yellow',
+				textColor: 'black',
+			    editable: true,
+			},
+	    ],
+
+        */
+
+        //修改事件
+        eventClick: function(event, element, view) {
         	/*
-        	var title1, title2
-
-        	bootbox.prompt({
-				  size: "small",
-				  title: "團名變更", 
-				  callback: function(result){
-				  	title1 = result
-
-					event.title = title1+"\n"+title2
-
-					console.log(event.title)
-				  }
-			})
-			bootbox.prompt({
-				  size: "small",
-				  title: "姓名變更", 
-				  callback: function(result){
-				  	title2 = result
-				  }
-			})
-
-
+        	var new_title = prompt('編輯', event.title)
+	        event.title = new_title
 	        $('#calendar').fullCalendar('updateEvent', event)
-
-	        $(this).contextmenu(function(e){
-		        //e.preventDefault()
-		        /*
-		        bootbox.confirm({
-				    message: "是否刪除?",
-				    buttons: {
-				        confirm: {
-				            label: '是',
-				            className: 'btn-success'
-				        },
-				        cancel: {
-				            label: '否',
-				            className: 'btn-danger'
-				        }
-				    },
-				    callback: function (result) {
-				    	if(result==true) r = "是"
-						else r = "否"
-
-				        console.log('[訊息] ' + r)
-				    }
-				})
-				
-		    })
-    		*/
-		    //console.log(Date())
+	        console.log(event.title)
+	        */
 			return false
 	    },
 
@@ -315,7 +179,8 @@ $(document).ready(function() {
 	    	//start
 	    	slt_st = start._i
 	    	slt_st = slt_st.toString().split(",")
-			//console.log(slt_st)
+
+	    	//console.log(slt_st)
 
 	    	slt_yr_st = slt_st[0]
 	    	slt_mth_st = (parseInt(slt_st[1])+1).toString()
@@ -399,6 +264,7 @@ $(document).ready(function() {
 	    	
 
 
+
 	    	//console.log(Select_Events_Start)
 	    	//console.log(Select_Events_End)
 
@@ -406,7 +272,7 @@ $(document).ready(function() {
 	    	//newBand_Name = bootbox.prompt('團名', function(result){})
 
 	    	
-	    	//輸入資料
+
 			bootbox.prompt({
 				  size: "small",
 				  title: "團名",
@@ -430,66 +296,24 @@ $(document).ready(function() {
 
 				  	if(newBand_Name==null||newBand_Name==''){}
 			    	else{
-			    		
-			    		/*
-			    		EventHeader = newBand_Name
-			    		Timestamps = addEventTimestamps
-			    		Bands_Name = newBand_Name
-			    		Names = newName
-			    		Date_Times = RegisterTime
-			    		Start_Times = Select_Events_Start
-			    		End_Times = Select_Events_End
 
-						//console.log(EventHeader, Timestamps, Bands_Name, Names, Date_Times, Start_Times, End_Times)
-						$addEventToCalendar(EventHeader, Timestamps, Bands_Name, Names, Date_Times, Start_Times, End_Times)
-						*/
-
+  						//addEventToCalendar(EventHeader, Timestamps, Bands_Name, Names, Date_Times, Start_Times, End_Times)
+			    	
 						Select_Events.push(
 						{
-							title: newBand_Name+"\n"+newName,
+							title: newBand_Name,
 						    start: Select_Events_Start,
 						    end:   Select_Events_End,
 						    color: color[random(0, color.length-1)],
 							textColor: 'black',
-						    editable: true,
+						    editable: false,
 						})
 
 						//console.log(Select_Events[0])
 
 						$('#calendar').fullCalendar( 'addEventSource', Select_Events )
-
-						Select_Events = [] //清除事件快取
-
-						//addEventTimestamps = ''
-
-						newStart = slt_st_full
-						newEnd = slt_end_full
-						newTime = addEventTimestamps
-
-						newData = {
-							  "method": "write",
-							  "Timestamps": newTime,
-							  "Bands_Name": newBand_Name,
-							  "Names": newName,
-							  "Date_Times": RegisterTime,
-							  "Start_Times": newStart,
-							  "End_Times": newEnd
-						}
-
-						//console.log("[送出資料] :"), console.log(newData)
-
-						$.ajax({
-				    		url: GoogleAppScript,
-				    		type: "POST",
-				    		data: newData,
-				    		success: function(result){
-				    			console.log(result)
-				    		}
-				    	})
-				    	console.log('[訊息] '+newTime+' 已儲存！')
 						
 					}
-
 				  }
 
 			})
@@ -504,10 +328,11 @@ $(document).ready(function() {
 	    },
 
 
-	    //移動
+	    /*
 	    eventDestroy: function( event, element, view ){
-			//console.log(event.title)
+			console.log(event);
 		},
+		*/
 
 	    //獲取時間
 	    /*
@@ -533,8 +358,6 @@ $(document).ready(function() {
 
 	AjaxUrl_2 = "https://spreadsheets.google.com/feeds/cells/"+spreadsheetId+"/"+sheet_flag_1+"/public/values?alt=json"
 
-	//https://spreadsheets.google.com/feeds/cells/1yS-MW2BMESK6o5_-qYwzNi1BPu7kmtx3w7JqAF1wO0M/1/public/values?alt=json
-
 	AjaxUrl_3 = "https://spreadsheets.google.com/feeds/cells/"+spreadsheetId+"/"+sheet_flag_2+"/public/values?alt=json"
 
 	Json = '' //json
@@ -545,7 +368,6 @@ $(document).ready(function() {
 	Start_Times = [] //起始時間
 	End_Times = [] //結束時間
 
-	//獲取Excel資料
 	$.ajax({
 		url: AjaxUrl_2,
 		method: "GET",
@@ -629,12 +451,12 @@ $(document).ready(function() {
 		}
 
 		console.log("[訊息] 載入資料中...")
-		//console.log(Timestamps)
-		//console.log(Bands_Name)
-		//console.log(Names)
-		//console.log(Date_Times)
-		//console.log(Start_Times)
-		//console.log(End_Times)
+		console.log(Timestamps)
+		console.log(Bands_Name)
+		console.log(Names)
+		console.log(Date_Times)
+		console.log(Start_Times)
+		console.log(End_Times)
 
 		EventHeader = Bands_Name
 
@@ -643,32 +465,89 @@ $(document).ready(function() {
 		}
 
 	}).done(function(){
+
 		console.log('[訊息] Google Excel 資料載入成功!')
+
+		//儲存行事曆 送出表單
+	    $('.fc-left').append('<button type="button" class="save-calendar fc-button fc-state-default fc-corner-left fc-corner-right">儲存</button>')
+
+	    $('.fc-button').mouseenter(function(){$(this).addClass('fc-state-hover')})
+	    $('.fc-button').mouseleave(function(){$(this).removeClass('fc-state-hover')})
+
+	    $('.fc-button').mousedown(function(){$(this).addClass('fc-state-down')})
+	    $('.fc-button').mouseup(function(){$(this).removeClass('fc-state-down')})
+
+
+
+	    addEventTimestamps = ''
+	    $('.save-calendar').click(function(){
+	    	
+			newStart = slt_st_full
+			newEnd = slt_end_full
+			newTime = addEventTimestamps
+
+			newData = {
+				  "Timestamps": newTime,
+				  "Bands_Name": newBand_Name,
+				  "Names": newName,
+				  "Date_Times": RegisterTime,
+				  "Start_Times": newStart,
+				  "End_Times": newEnd
+			}
+
+			console.log("[送出資料] :" + newData)
+
+			/*
+			///////google api
+
+			//old
+			//Sheets_Append_Url = "https://sheets.googleapis.com/v4/spreadsheets/"+spreadsheetId+"/values/A1:append?valueInputOption="+USER_ENTERED+"&key="+API_KEY
+
+			//new
+			//Sheets_Append_Url = "https://sheets.googleapis.com/v4/spreadsheets/"+spreadsheetId+"/values/Sheet1!A1:append?valueInputOption=USER_ENTERED"
+
+			*/
+
+			GoogleAppScript = "https://script.google.com/macros/s/AKfycbwXe1UlNdLZErA5ouR45DDPCw9ZctBj9CIoahuQL6rhvcTPCTJj/exec"
+			
+			$.ajax({
+	    		url: GoogleAppScript,
+	    		method: "POST",
+	    		data: newData,
+	    		success: function(result){
+	    			console.log(result)
+	    		}
+	    	})
+	    	console.log('[訊息] '+newTime+' 行事曆已儲存！')
+	    })
+
 	})
 
 
 
-	//addEventToCalendar新增事件到行事曆
-	$addEventToCalendar = function(EventHeader, Timestamps, Bands_Name, Names, Date_Times, Start_Times, End_Times)
-	{
 
-		Events_Array = []
-		for(i=0 ; i<EventHeader.length ; i++){
-			Events_Array[i] = {
-				//"\n" "\r"
-				title: Bands_Name[i]+"\n"+Names[i],
-			    start: Date_Times[i]+'T'+Start_Times[i],
-			    end:   Date_Times[i]+'T'+End_Times[i],
-			    color: color[random(0, color.length-1)],
-				textColor: 'black',
-			    editable: true,
-			}
+	
+
+//addEventToCalendar新增事件到行事曆
+$addEventToCalendar = function(EventHeader, Timestamps, Bands_Name, Names, Date_Times, Start_Times, End_Times){
+	Events_Array = []
+	for(i=0;i<EventHeader.length;i++){
+		Events_Array[i] = {
+			//"\n" "\r"
+			title: Bands_Name[i]+"\n"+Names[i],
+		    start: Date_Times[i]+'T'+Start_Times[i],
+		    end:   Date_Times[i]+'T'+End_Times[i],
+		    color: color[random(0, color.length-1)],
+			textColor: 'black',
+		    editable: false,
+
+		    //
 		}
-
-		//$('#calendar').fullCalendar( 'renderEvents', Events_Array, true );
-		$('#calendar').fullCalendar( 'addEventSource', Events_Array )
-
 	}
+
+	//$('#calendar').fullCalendar( 'renderEvents', Events_Array, true );
+	$('#calendar').fullCalendar( 'addEventSource', Events_Array )
+}
 
 	
 });
