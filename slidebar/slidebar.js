96b22a581646b5623dc9ae70e98fb99317ce21b6
nuupopmusic <nuupopmusic@gmail.com>
2021-07-25 22:34:34 +0800

    'update'

---
+++ slidebar/slidebar.js
@@ -0,0 +1,15 @@
+﻿const slidebar = () => {
+  console.log("slidebar")
+
+  var overlay = document.querySelector(".overlay")
+  var menu = document.querySelector(".menu")
+  var menuBtn = document.querySelector("#menuBtn")
+
+  menuBtn.addEventListener("click", () => {
+    overlay.classList.toggle("active")
+    menu.classList.toggle("active")
+  })
+
+}
+
+window.addEventListener("DOMContentLoaded", slidebar)
